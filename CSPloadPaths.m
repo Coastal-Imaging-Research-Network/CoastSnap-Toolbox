b18343eceab1eeb169415f0e76916d4f625a0240
mitchharley <m.harley@unsw.edu.au>
2018-06-25 14:34:22 +1000

    Added support routines path link

--- CSPloadPaths.m
+++ CSPloadPaths.m
@@ -13,5 +13,7 @@ tide_path = fullfile(base_path,'Tide Data'); %Path where tide data are stored
 transect_dir = fullfile(base_path,'Shorelines','Transect Files'); %Path where transects are stored for shoreline mapping
 
 %Add relevant paths to Matlab
+support_routines_path = 'C:\Users\z2273773\OneDrive - UNSW\RESEARCH2\GitHub\Support-Routines'; %Path where support routines are stored (e.g. matlab2Epoch)
 shoreline_mapping_toolbox_path = 'C:\Users\z2273773\OneDrive - UNSW\RESEARCH2\GitHub\CoastSnap-Toolbox'; %Path where the shoreline mapping toolbox is located
+addpath(support_routines_path)
 addpath(shoreline_mapping_toolbox_path)
