0bfb57b4068212583fbe59e17071dc4f74a28f56
mitchharley <m.harley@unsw.edu.au>
2018-10-30 11:03:51 +1100

    Added new file to set paths

---
+++ CSPsetPaths.m
@@ -0,0 +1,3 @@
+%Add relevant paths to Matlab
+github_path = 'C:\Users\z2273773\OneDrive - UNSW\RESEARCH2\GitHub'; %Path where CIRN toolboxes (including coastsnap) are located
+addpath(genpath(github_path))
