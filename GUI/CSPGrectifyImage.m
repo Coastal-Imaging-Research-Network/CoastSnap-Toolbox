7da4e16140e43543de1dfbb4877fbf69a939d4a3
mitchharley <m.harley@unsw.edu.au>
2019-06-15 03:31:20 +1000

    Removed hard-wired gcp_list

--- GUI/CSPGrectifyImage.m
+++ GUI/CSPGrectifyImage.m
@@ -1,7 +1,8 @@
-function out = CSPGrectifyImage(handles,gcp_list)
+function out = CSPGrectifyImage(handles)
 
 data = get(handles.oblq_image,'UserData'); %Get data stored in the userdata in the oblq_image handles
 siteDB = data.siteDB;
+gcp_list = CSPgetGCPcombo(siteDB,data.epoch);
 I = data.I;
 axes(handles.oblq_image) %Plot gpcs on GUI axis
 
