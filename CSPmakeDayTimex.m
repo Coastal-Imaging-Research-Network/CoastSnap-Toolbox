4e1b0cff5b9a683363d7e7acbdac6f16079f6687
Mitchell Harley <z2273773@ad.unsw.edu.au>
2021-01-11 12:37:38 +1100

    Fixed bug in makedaytimex

--- CSPmakeDayTimex.m
+++ CSPmakeDayTimex.m
@@ -8,17 +8,18 @@ matlabdays = matlabday1:matlabday2;
 for i = 1:length(matlabdays)
     disp(['Doing days ' num2str(i) ' of ' num2str(length(matlabdays))])
     Iday = find(floor(matlabtimes)==matlabdays(i));
-    if length(Iday>2)
-        for j = 1:length(Iday)
-            Snap = uint32(imread(fullfile(filepaths(Iday(j)).name,filenames(Iday(j)).name)));
-            II = imadd(Snap,uint32(imread(fullfile(filepaths(Iday(j)).name,filenames(Iday(j)).name))));
+    if length(Iday)>2
+        II = uint32(imread(fullfile(filepaths(Iday(1)).name,filenames(Iday(1)).name)));
+        for j = 2:length(Iday)
+            %Snap = uint32(imread(fullfile(filepaths(Iday(j)).name,filenames(Iday(j)).name)));
+            II = imadd(II,uint32(imread(fullfile(filepaths(Iday(j)).name,filenames(Iday(j)).name))));
         end
         I = imdivide(II,length(Iday));
         fparts = CSPparseFilename(filenames(Iday(j)).name);
         epochtime = matlab2Epoch(matlabdays(i)+siteDB.timezone.gmt_offset/24);
         fparts.type = 'daytimex';
         newname = CSPargusFilename(epochtime,site,-1,fparts.type,fparts.user,'jpg');
-        imwrite(uint8(I),newname,'jpg','quality',100);
+        imwrite(uint8(I),fullfile(filepaths(Iday(j)).name,newname),'jpg','quality',100);
     end
 end
 
