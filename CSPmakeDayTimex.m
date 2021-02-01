2a7c73109e319f9de791a358175cb00f7ce6a0bd
mitchharley <m.harley@unsw.edu.au>
2021-02-01 16:12:31 +1100

    Added script to make daytimex

---
+++ CSPmakeDayTimex.m
@@ -0,0 +1,30 @@
+function CSPmakeDayTimex(site,matlabday1,matlabday2)
+
+[epochtimes,filenames,filepaths,tide_levels] = CSPgetImageList(site,'Processed');
+siteDB = CSPreadSiteDB(site);
+matlabtimes = epoch2Matlab(epochtimes)+siteDB.timezone.gmt_offset/24; %Convert to local time
+matlabtimes_vec = datevec(matlabtimes);
+I = find(ismember(matlabtimes_vec(:,4),[7:18])); %Only use images between 8am and 6pm
+matlabtimes = matlabtimes(I);
+matlabdays = matlabday1:matlabday2;
+
+for i = 1:length(matlabdays)
+    disp(['Doing days ' num2str(i) ' of ' num2str(length(matlabdays))])
+    Iday = find(floor(matlabtimes)==matlabdays(i));
+    if length(Iday)>2
+        II = uint32(imread(fullfile(filepaths(Iday(1)).name,filenames(Iday(1)).name)));
+        for j = 2:length(Iday)
+            %Snap = uint32(imread(fullfile(filepaths(Iday(j)).name,filenames(Iday(j)).name)));
+            II = imadd(II,uint32(imread(fullfile(filepaths(Iday(j)).name,filenames(Iday(j)).name))));
+        end
+        I = imdivide(II,length(Iday));
+        fparts = CSPparseFilename(filenames(Iday(j)).name);
+        epochtime = matlab2Epoch(matlabdays(i)+siteDB.timezone.gmt_offset/24);
+        fparts.type = 'daytimex';
+        newname = CSPargusFilename(epochtime,site,-1,fparts.type,fparts.user,'jpg');
+        imwrite(uint8(I),fullfile(filepaths(Iday(j)).name,newname),'jpg','quality',100);
+    end
+end
+
+    
+    
