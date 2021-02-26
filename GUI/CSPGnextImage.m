62d00943a380fb91c6bed8610a98b17361b2e283
mitchharley <m.harley@unsw.edu.au>
2021-02-26 13:33:59 +1100

    Fixed bug to navigate images

--- GUI/CSPGnextImage.m
+++ GUI/CSPGnextImage.m
@@ -5,8 +5,13 @@ data = get(handles.oblq_image,'UserData');
 
 %Find present image in set of images all within a similar tide
 I = find(data.navigation.epochs==data.epoch);
-newpath = data.navigation.paths(I+1).name; %Step one forward
-newfile = data.navigation.files(I+1).name; %Step one forward
+if I<length(data.navigation.epochs)
+    newpath = data.navigation.paths(I+1).name; %Step one forward
+    newfile = data.navigation.files(I+1).name; %Step one forward
+    
+    %Reload data to GUI with new image path and file
+    CSPGloadImage(handles,newpath,newfile);
+else
+    disp('You have reached the final image. Please stop pressing next for everyone''s sake')
+end
 
-%Reload data to GUI with new image path and file
-CSPGloadImage(handles,newpath,newfile);
--- GUI/CSPGpreviousImage.m
+++ GUI/CSPGpreviousImage.m
@@ -5,8 +5,13 @@ data = get(handles.oblq_image,'UserData');
 
 %Find present image in set of images all within a similar tide
 I = find(data.navigation.epochs==data.epoch);
-newpath = data.navigation.paths(I-1).name; %Step one back
-newfile = data.navigation.files(I-1).name; %Step one back
-
-%Reload data to GUI with new image path and file
-CSPGloadImage(handles,newpath,newfile);
+if I>1
+    newpath = data.navigation.paths(I-1).name; %Step one back
+    newfile = data.navigation.files(I-1).name; %Step one back
+    
+    %Reload data to GUI with new image path and file
+    CSPGloadImage(handles,newpath,newfile);
+else
+    disp('You have stepped to the first image')
+end
+ 
